20ee377144829ffbba0a50f8c8789b987f243042
andersonMiranda <and.miranda1818@gmail.com>
2026-02-26 21:40:22 -0300

    refactor: improve footer layout and adjust section scroll behavior

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,30 +1,61 @@
-import { Instagram, Mail, Phone } from "lucide-react";
+"use client"
+
+import { House, Lightbulb, Mail, Phone, ShieldCheck, Sparkle } from "lucide-react";
 import Logo from "../domain/logo";
 import Text from "../ui/text";
+import Link from "next/link";
+import { useIsHome } from "@/hook/useIsHome";
 
 const Footer = () => {
+  const isHome = useIsHome();
+  const aboutLink = isHome ? "#about_us" : "/#about_us";
+  const servicesLink = isHome ? "#services" : "/#services";
   return (
     <section
-      className="flex flex-col items-center w-full gap-7 py-5 px-6 bg-black text-white lg:grid lg:grid-cols-3"
+      className="flex flex-col items-center w-full gap-7 py-8 px-6 bg-black text-white lg:grid lg:grid-cols-3"
       id="contacts"
     >
       <div className="flex justify-self-start">
         <Logo />
       </div>
 
-      <div className="justify-self-center flex flex-col gap-2 font-title">
+      <div className="justify-self-center">
+        <ul className="flex flex-col gap-3">
+          <li className="flex items-center gap-2 transition-all duration-300 hover:text-secondary">
+            <House size={15} />
+            <Link href={aboutLink}>Nossa história</Link>
+          </li>
+          <li className="flex items-center gap-2 transition-all duration-300 hover:text-secondary">
+            <ShieldCheck size={15} />
+            <Link href={servicesLink}>Serviços</Link>
+          </li>
+
+          <li className="flex items-center gap-2 transition-all duration-300 hover:text-secondary">
+            <ShieldCheck size={15} />
+            <Link href={"/premium_services"}>Serviços Premium</Link>
+          </li>
+          <li className="flex items-center gap-2 transition-all duration-300 hover:text-secondary">
+            <Sparkle size={15} />
+            <Link href={"/beard_tips"}>Dicas para fazer a barba</Link>
+          </li>
+          <li className="flex items-center gap-2 transition-all duration-300 hover:text-secondary">
+            <Lightbulb size={15} />
+            <Link href={"/recommendations"}>
+              Recomendações de produtos para sua barba
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col gap-2 font-title justify-self-end cursor-default">
         <div className="flex items-center gap-2">
-          <Phone size={17}/>
+          <Phone size={17} />
           <Text size="lg">{`(11) 4341-6585`}</Text>
         </div>
         <div className="flex items-center gap-2">
-          <Mail size={17}/>
+          <Mail size={17} />
           <Text>barberbox@gmail.com</Text>
         </div>
-      </div>
-
-      <div className="flex justify-self-end cursor-pointer">
-        <Instagram />
       </div>
     </section>
   );
--- src/components/sections/serviceListClient.tsx
+++ src/components/sections/serviceListClient.tsx
@@ -12,7 +12,7 @@ const ServiceListClient = ({ services, onSelectService }: Props) => {
 
 
   return (
-    <section className="grid grid-cols-1 gap-x-4 gap-y-5 md:grid-cols-2 xl:grid-cols-3" id="services">
+    <section className="grid grid-cols-1 gap-x-4 gap-y-5 md:grid-cols-2 xl:grid-cols-3">
       {services.map((service) => (
         <ServiceCard key={service.id} {...service} onClick={() => onSelectService(service.id)} isPremium={service.tag === 'premium'} />
       ))}
