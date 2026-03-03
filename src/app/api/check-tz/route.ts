781accca2f1cfb9349da9e573aa455ba1bcd52f7
andersonMiranda <and.miranda1818@gmail.com>
2026-03-02 23:25:53 -0300

    fix: finish test

--- src/app/api/check-tz/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  return Response.json({
-    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-    offset: new Date().getTimezoneOffset(),
-    now: new Date().toString(),
-  });
-}
